79abab36fc38da02f617a2585007721be721ba34
GiSeong Lee <cj8928@gmail.com>
2024-07-22 19:53:25 +0900

    update: card id alert 및 dom 생성 함수화

--- js/card.js
+++ js/card.js
@@ -9,38 +9,53 @@ const makeDataToCards = async () => {
   cardContainer.appendChild(cardList);
   root.appendChild(cardContainer);
   try {
-    const data = await getTopRatedMoviesList();
-    const cardData = data.results;
-    console.log(cardData);
-
-    const html = cardData
-      .map((element, index) => {
-        return `
-        <li class="card-list-contents">
-          <div class="card-box" key=${index}>
-            <div class="card-image">
-                <img src="https://image.tmdb.org/t/p/w200${element.poster_path}" alt="..."/>
-            </div>
-            <h3 class="card-title">
-              ${element.title}
-            </h3>
-            <div class="card-content">
-              ${element.overview}
-            </div>
-            <div class="card-vote">
-              ${element.vote_average}
-            </div>
-          </div>
-        </li>
-      `;
-      })
-      .join("");
-    document
-      .querySelectorAll("#card-list")
-      .forEach((e) => (e.innerHTML = html));
+    const html = await makeCards();
+    cardList.innerHTML = html;
+    cardContainer.addEventListener("click", (event) => {
+      const cardBox = event.target.closest(".card-box");
+      if (cardBox) {
+        const id = cardBox.getAttribute("key");
+        handleCardAlert(id);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
+};
+
+const handleCardAlert = (id) => {
+  alert(`영화 id: ${id}`);
+};
+
+export const makeCards = async (keyword) => {
+  const data = await getTopRatedMoviesList();
+  const cardData = keyword
+    ? data.results.filter((e) => e.title.includes(keyword))
+    : data.results;
+
+  const html = cardData
+    .map((element) => {
+      return `
+          <li class="card-list-contents">
+            <div class="card-box" key=${element.id}>
+              <div class="card-image">
+                  <img src="https://image.tmdb.org/t/p/w200${element.poster_path}" alt="..."/>
+              </div>
+              <h3 class="card-title">
+                ${element.title}
+              </h3>
+              <div class="card-content">
+                ${element.overview}
+              </div>
+              <div class="card-vote">
+                평점: ${element.vote_average}
+              </div>
+            </div>
+          </li>
+        `;
+    })
+    .join("");
+  return html;
 };
 
 export default makeDataToCards;
